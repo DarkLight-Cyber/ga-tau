32ed22f17d50be43218c08782e7ae24c56f25c80
DarkLight-Cyber <56876737+DarkLight-Cyber@users.noreply.github.com>
2019-10-22 21:53:03 +0700

    Add files via upload

---
+++ 4GLTE.sh
@@ -0,0 +1,45 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay by darklight
+echo $green"SELAMAT DATANG!!"
+echo
+sleep 2
+echo $green"Silahkan pilih tool :"
+echo
+echo $cyan"1.penambah ping"
+echo
+echo $red"0.exit"
+echo
+echo $white
+read -p "pilih yang mana? :" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay by Dark Light
+echo $grenn"menambahkan ping . . ."
+sleep 3
+ping -s2000 1.1.1.1
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+echo $black
+figlet "Dark"
+echo $blue
+figlet "light"
+echo $yellow"to be continued"
+sleep 4
+https://www.youtube.com/channel/UCRmCU89t6yhrrzuHejBzw2Q
+exit
+fi
+
